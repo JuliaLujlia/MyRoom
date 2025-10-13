96abd63329af9901a84ed60e9cbc7ab5c0033b91
Julia Egyed <juliateddy5@gmail.com>
2025-10-13 20:31:21 +0200

    sounds hinzugefügt

--- Assets/bag.cs
+++ Assets/bag.cs
@@ -8,17 +8,29 @@ public class bag : MonoBehaviour
     private bool isVisible = false;
     public Text speechText;
     public AudioSource soundSource;
+    public AudioSource zipSound;
 
     private void OnMouseDown()
     {
         speechText.text = "Hobbys habe ich natürlich auch… also wenigstens ein paar.";
         isVisible = true;
-        speechBubble.SetActive(isVisible);
-        soundSource.Play();
+
+        // Starte Coroutine, um Sounds nacheinander abzuspielen
+        StartCoroutine(PlaySoundsSequentially());
+
         Debug.Log("Gedrückt");
         StartCoroutine(HideAfterSeconds(5f));
     }
 
+    private IEnumerator PlaySoundsSequentially()
+    {
+        soundSource.Play();
+
+        // Warte, bis soundSource fertig ist
+        yield return new WaitWhile(() => soundSource.isPlaying);
+
+        zipSound.Play();
+    }
 
     private IEnumerator HideAfterSeconds(float delay)
     {
