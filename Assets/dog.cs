65a5d9eee43960fe76a813fe0de21df3c04a7ce5
Julia Egyed <juliateddy5@gmail.com>
2025-10-15 21:37:54 +0200

    script sound

--- Assets/dog.cs
+++ Assets/dog.cs
@@ -9,15 +9,29 @@ public class dog : MonoBehaviour
     private bool isVisible = false;
     public Text speechText;
     public AudioSource soundSource;
+    public AudioSource dogSound;
 
     private void OnMouseDown()
     {
         speechText.text = "Kann mir jemand sagen, wer hier eigentlich wen spazieren führt?";
         isVisible = true;
         speechBubble.SetActive(isVisible);
-        soundSource.Play();
+
+        // Starte Coroutine, um Sounds nacheinander abzuspielen
+        StartCoroutine(PlaySoundsSequentially());
+
         Debug.Log("Gedrückt");
         StartCoroutine(HideAfterSeconds(5f));
+    }
+
+    private IEnumerator PlaySoundsSequentially()
+    {
+        soundSource.Play();
+
+        // Warte, bis soundSource fertig ist
+        yield return new WaitWhile(() => soundSource.isPlaying);
+
+        dogSound.Play();
     }
 
 
